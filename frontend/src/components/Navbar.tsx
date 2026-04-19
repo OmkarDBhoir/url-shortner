4bbb8da72ecdc3d77bbf9383c63e52176dddfa73
omkar <omkar123b@gmail.com>
2026-04-19 20:49:58 +0530

    Added UI changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,18 @@
+import type { FC } from "react";
 import './styles.css';
 
-const Navbar:React.FC = () => {
-    return(
-        <>
-        <div className='navbar'>
-            <div className='title'>SHORTNER</div>
-        </div>
-        </>
+const Navbar: FC = () => {
+    return (
+        <header className="navbar">
+            <div className="navbar-brand">
+                <div className="brand-logo">URL</div>
+                <div className="brand-name">Shortener</div>
+            </div>
+            <div className="navbar-actions">
+                <button className="ghost-button">Sign Up</button>
+                <button className="primary-button">Log In</button>
+            </div>
+        </header>
     )
 }
 
