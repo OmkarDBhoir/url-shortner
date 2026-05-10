f729f79fcbe46b87890f81cf36cb6a1ccfd2415f
omkar <omkar123b@gmail.com>
2026-05-10 12:44:11 +0530

    Added changes

--- frontend/src/components/constants/baseContants.ts
+++ frontend/src/components/constants/baseContants.ts
@@ -1,4 +1,5 @@
 
 export class BaseConstant {
-    static baseUrl:string = "http://localhost:8080";
+    // static baseUrl:string = "http://localhost:8080";
+    static baseUrl:string = import.meta.env.VITE_API_URL;
 }
--- shorturl/src/main/java/com/shorturl/shorturl/ShorturlApplication.java
+++ shorturl/src/main/java/com/shorturl/shorturl/ShorturlApplication.java
@@ -3,11 +3,18 @@ package com.shorturl.shorturl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.shorturl.shorturl.constants.UrlServiceConstants;
+import com.shorturl.shorturl.utils.FLogger;
+
 @SpringBootApplication
 public class ShorturlApplication {
+	
+	private static final String LOG_CAT = UrlServiceConstants.LOGGER_CONSTANT;
+	private static final String THIS_CLASS = ShorturlApplication.class.getName();
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShorturlApplication.class, args);
+		FLogger.error(LOG_CAT, THIS_CLASS, "Main", ShorturlApplication.class.getSimpleName() + " started.");
 	}
 
 }
